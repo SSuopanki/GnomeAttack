8f725c40cb5ac214b4ad06d4e1972edf7dc4f642
SSuopanki <77267467+SSuopanki@users.noreply.github.com>
2021-11-09 11:55:04 +0200

    Update LevelProgression.cs

--- LevelProgression.cs
+++ LevelProgression.cs
@@ -10,14 +10,12 @@ public class LevelProgression : MonoBehaviour
     //private GameObject LevelButtonArr;
     public LevelSelect levelSel;
     private string sceneName;
-    private string sceneNum;
     private int LevelNum;
     
     void Start() 
     {
-        sceneName = SceneManager.GetActiveScene().name;
-        sceneNum = sceneName.Substring(6);
-        LevelNum = int.Parse(sceneNum);
+        sceneName = SceneManager.GetActiveScene().name.Substring(6);
+        LevelNum = int.Parse(sceneName);
 
 
 
