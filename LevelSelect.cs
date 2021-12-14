947f89779eefef236719ab5579866a8be166d88c
SSuopanki <77267467+SSuopanki@users.noreply.github.com>
2021-12-14 13:00:16 +0200

    Update LevelSelect.cs

--- LevelSelect.cs
+++ LevelSelect.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class LevelSelect : MonoBehaviour
@@ -9,13 +11,16 @@ public class LevelSelect : MonoBehaviour
     public GameObject levelHolder;
     public GameObject levelIcon;
     public GameObject thisCanvas;
-    public int numberOfLevels = 50;
+    public int numberOfLevels = 40;
     public Vector2 iconSpacing;
     private Rect panelDimensions;
     private Rect iconDimensions;
     private int PerPage;
     private int currentLevelCount;
     public GameObject[] levelButtons;
+    private string sceneName;
+    private int LevelNum;
+    public LevelSelect levelSel;
 
 
     // Calculations how many panels are needed  
@@ -29,19 +34,25 @@ public class LevelSelect : MonoBehaviour
         int totalPages = Mathf.CeilToInt((float)numberOfLevels / PerPage);
         LoadPanels(totalPages);
 
+        levelSel = Camera.main.GetComponent<LevelSelect>();
+        int CompletedLevels = PlayerPrefs.GetInt("CompletedLevels"); //level progression
+        int levelReached = CompletedLevels + 1;
 
+        //LevelSelect LvlButton = LevelButtonArr.AddComponent<LevelSelect>();
 
-        //level progress
-
-        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+        Debug.Log(levelButtons);
 
         for (int i = 0; i < levelButtons.Length; i++)
         {
             if (i + 1 > levelReached)
             {
+                GameObject img = levelButtons[i].gameObject.transform.GetChild(0).gameObject;
+                img.GetComponent<Image>().color = new Color32(255,255,225,100);
                 levelButtons[i].GetComponent<Button>().interactable = false;
             }
         }
+
+
     }
 
     // Total panels
@@ -77,14 +88,14 @@ public class LevelSelect : MonoBehaviour
     {
         for (int i = 1; i <= numberOfIcons; i++)
         {
-           
+
             currentLevelCount++;
             GameObject icon = Instantiate(levelIcon) as GameObject;
             icon.transform.SetParent(thisCanvas.transform, false);
             icon.transform.SetParent(parentObject.transform);
             icon.name = "Level_" + currentLevelCount;
             icon.GetComponentInChildren<TextMeshProUGUI>().SetText(currentLevelCount.ToString());
-            levelButtons[currentLevelCount-1] = icon;
+            levelButtons[currentLevelCount - 1] = icon;
 
         }
 
