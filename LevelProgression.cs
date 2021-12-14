2ea640df4bf4f551f8a3a0db95d6789fd1b2bb16
SSuopanki <77267467+SSuopanki@users.noreply.github.com>
2021-12-14 13:02:15 +0200

    Update LevelProgression.cs

--- LevelProgression.cs
+++ LevelProgression.cs
@@ -3,38 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
 public class LevelProgression : MonoBehaviour
 {
-
     //private GameObject LevelButtonArr;
     public LevelSelect levelSel;
     private string sceneName;
     private int LevelNum;
-    
-    void Start() 
+    void Start()
     {
         sceneName = SceneManager.GetActiveScene().name.Substring(6);
         LevelNum = int.Parse(sceneName);
-
-
-
+        
         levelSel = Camera.main.GetComponent<LevelSelect>();
         int levelReached = PlayerPrefs.GetInt("levelReached", 1); //level progression
-
         //LevelSelect LvlButton = LevelButtonArr.AddComponent<LevelSelect>();
-
         Debug.Log(levelSel.levelButtons);
-
         for (int i = 0; i < levelSel.levelButtons.Length; i++)
         {
             if (i + 1 > levelReached)
             {
                 levelSel.levelButtons[i].GetComponent<Button>().interactable = false;
-
             }
         }
-
     }
-    // lisää vicotry kohtaan PlayerPrefs.SetInt("levelReached", currentlevel + 1 );
 }
